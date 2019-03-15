f813aa84b7956c38c31fc64f44a6dba8b6ac063f
Julie <dav15036@byui.edu>
2019-03-15 16:20:18 -0600

    Added create new user pop up to profile

--- app/src/main/java/com/example/project_management_app/Profile_page.java
+++ app/src/main/java/com/example/project_management_app/Profile_page.java
@@ -1,13 +1,32 @@
 package com.example.project_management_app;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile_page extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+    myDialog= new Dialog(this);
+    }
+    public void CreatePopup(View v){
+        TextView close;
+        Button createUser;
+        myDialog.setContentView(R.layout.new_user_popup);
+        close = (TextView) myDialog.findViewById(R.id.close);
+        createUser = (Button) myDialog.findViewById(R.id.createUser);
+        close.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
     }
 }
