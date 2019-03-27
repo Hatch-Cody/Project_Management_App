3c0bb7f3ee6c4f3839c45d5f02e97bf6dc6ed1c1
Cody <hat15008@byui.edu>
2019-03-27 09:54:44 -0600

    Worked on the Tasks_newTasks Class, TaskAdapter Class, and the new_task_popup XML. I made some changes dealing with the database and trying to get the app to read and write to it. Changes were made to the TaskAdapter to try to get it to function properly.

--- app/src/main/java/com/example/project_management_app/tasks_newTask.java
+++ app/src/main/java/com/example/project_management_app/tasks_newTask.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -113,7 +115,6 @@ public class tasks_newTask extends AppCompatActivity {
     // Reads the information from the popup into the database
     public void uploadTask() {
 
-        /*
         Log.d(TAG, "uploadTask() called");
 
         createTaskButton = (Button) findViewById(R.id.createTaskButton);
@@ -234,8 +235,6 @@ public class tasks_newTask extends AppCompatActivity {
                 }
             }
         });
-        */
-
     }
 
     @Override
